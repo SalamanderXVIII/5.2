48ec7b451434cc2e38ec08b92da038129222fead
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2021-12-24 11:53:46 +0200

    Добавьте файлы проекта.

---
+++ 5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Library;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mas = new double[8, 3];
+            Console.WriteLine("Hello World!");
+            for (int r = 0; r < mas.GetLength(0); r++)
+            {
+                for (int c = 0; c < mas.GetLength(1); c++)
+                {
+                    Console.WriteLine("Введите значение m[" + r + ", " + c + "]: ");
+                    mas[r, c] = double.Parse(Console.ReadLine());
+                }
+            }
+            Library.Matrix.arif(mas);
+        }
+    }
+}
---
+++ Library/Matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Library
+{
+    public class Matrix
+    {
+        public static double arif(double[,] mas)
+        {
+            double summ = 0;
+            for (int r = 0; r < mas.GetLength(0); r++)
+            {
+                for (int c = 0; c < mas.GetLength(1); c++)
+                {
+                    summ += mas[r, c];
+                }
+            }
+            double srarif = summ / (mas.GetLength(0) * mas.GetLength(1));
+            return srarif;
+        }
+    }
+}
